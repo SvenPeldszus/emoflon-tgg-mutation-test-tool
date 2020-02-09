52ecc165203d4dad5f955391ff9a325b922b8938
Marcel Michels <m.michels@freenet.de>
2020-02-09 22:12:28 +0100

    Updated wizard

--- bundles/de.unikoblenz.emoflon.tgg.mutationtest.ui/src/de/unikoblenz/emoflon/tgg/mutationtest/ui/MutationTestSetupWizard.java
+++ bundles/de.unikoblenz.emoflon.tgg.mutationtest.ui/src/de/unikoblenz/emoflon/tgg/mutationtest/ui/MutationTestSetupWizard.java
@@ -1,11 +1,13 @@
 package de.unikoblenz.emoflon.tgg.mutationtest.ui;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-
+import de.unikoblenz.emoflon.tgg.mutationtest.MutationTestExecuter;
 import de.unikoblenz.emoflon.tgg.mutationtest.ui.pages.ConfigSelectionPage;
 import de.unikoblenz.emoflon.tgg.mutationtest.ui.pages.ProjectSelectionPage;
 import de.unikoblenz.emoflon.tgg.mutationtest.ui.pages.TestConfigurationPage;
@@ -13,14 +15,18 @@ import de.unikoblenz.emoflon.tgg.mutationtest.ui.pages.TestConfigurationSelectio
 
 public class MutationTestSetupWizard extends Wizard {
 
-    public MutationTestSetupWizard() {
-    }
+	public MutationTestSetupWizard() {
+	}
 
-    private TestConfigurationSelectionPage testConfigSelectionPage = new TestConfigurationSelectionPage();
-    private ProjectSelectionPage projectSelectionPage = new ProjectSelectionPage();
-    private ConfigSelectionPage configSelectionPage = new ConfigSelectionPage();
-    private TestConfigurationPage testConfigurationPage = new TestConfigurationPage();
+	private TestConfigurationSelectionPage testConfigSelectionPage = new TestConfigurationSelectionPage();
 
+	private ProjectSelectionPage projectSelectionPage = new ProjectSelectionPage();
+
+	private ConfigSelectionPage configSelectionPage = new ConfigSelectionPage();
+
+	private TestConfigurationPage testConfigurationPage = new TestConfigurationPage();
+
+	private MutationTestExecuter mutationTestRunner = new MutationTestExecuter();
 
 	@Override
 	public void addPages() {
@@ -29,28 +35,38 @@ public class MutationTestSetupWizard extends Wizard {
 		addPage(configSelectionPage);
 		addPage(testConfigurationPage);
 	}
-	
+
 	@Override
 	public IWizardPage getNextPage(IWizardPage currentPage) {
 //	    if (todo.isDone()) {
 //	       return specialPage;
 //	    }
-		if(currentPage == testConfigSelectionPage) {
-			testConfigSelectionPage.get
+		if (currentPage == testConfigSelectionPage) {
 			return projectSelectionPage;
 		}
-	    if (currentPage == projectSelectionPage) {
-	        return configSelectionPage;
-	    }
-	    if (currentPage == configSelectionPage) {
-	    	return testConfigurationPage;
-	    }
-	    return null;
+		if (currentPage == projectSelectionPage) {
+			return configSelectionPage;
+		}
+		if (currentPage == configSelectionPage) {
+			return testConfigurationPage;
+		}
+		return null;
 	}
 
-    @Override
-    public boolean performFinish() {
-        return true;
-    }
+	@Override
+	public boolean performFinish() {
+		// collect data
+		IProject testProject = projectSelectionPage.getSelectedProject();
+		
+		IFile launchConfigFile = configSelectionPage.getLaunchConfigFile();
+		
+		Integer iterations = Integer.valueOf(testConfigurationPage.getIterations().getText());
+		
+		Integer timeout = Integer.valueOf(testConfigurationPage.getTimeout().getText());
+
+		//call mutation test runner
+		mutationTestRunner.executeTests(testProject, launchConfigFile, iterations, timeout);
+		return true;
+	}
 
 }
