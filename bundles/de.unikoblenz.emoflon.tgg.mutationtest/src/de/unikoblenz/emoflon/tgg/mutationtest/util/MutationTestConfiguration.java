910f3d0f44dbd02e5089ce07180fdb675580fe4a
Marcel Michels <m.michels@freenet.de>
2020-03-22 20:43:33 +0100

    Added config representation for serialization

--- bundles/de.unikoblenz.emoflon.tgg.mutationtest/src/de/unikoblenz/emoflon/tgg/mutationtest/util/MutationTestConfiguration.java
+++ bundles/de.unikoblenz.emoflon.tgg.mutationtest/src/de/unikoblenz/emoflon/tgg/mutationtest/util/MutationTestConfiguration.java
@@ -1,28 +1,55 @@
 package de.unikoblenz.emoflon.tgg.mutationtest.util;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.debug.core.ILaunchConfiguration;
+import java.util.Arrays;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchManager;
 
 public class MutationTestConfiguration {
-	
+
 	private String label;
-	
+
 	private IProject project;
-	
+
 	private ILaunchConfiguration launchConfig;
-	
+
 	private Integer iterations;
-	
+
 	private Integer timeout;
 
-	public MutationTestConfiguration(String label, IProject project, ILaunchConfiguration launchConfig, Integer iterations,
-			Integer timeout) {
+	public MutationTestConfiguration(String label, IProject project, ILaunchConfiguration launchConfig,
+			Integer iterations, Integer timeout) {
 		this.label = label;
 		this.project = project;
 		this.launchConfig = launchConfig;
 		this.iterations = iterations;
 		this.timeout = timeout;
+	}
+
+	public MutationTestConfiguration(MutationTestSerializableConfig serializableConfig) {
+		this.label = serializableConfig.getLabel();
+
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		this.project = workspaceRoot.getProject(serializableConfig.getProjectName());
+
+		ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
+		try {
+			this.launchConfig = 
+					Arrays.asList(manager.getLaunchConfigurations()).stream()
+					.filter(config -> config.getName().equals(serializableConfig.getLaunchConfigName())).findFirst().get();
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.iterations = serializableConfig.getIterations();
+		this.timeout = serializableConfig.getTimeout();
+
 	}
 
 	public String getLabel() {
@@ -64,7 +91,5 @@ public class MutationTestConfiguration {
 	public void setTimeout(Integer timeout) {
 		this.timeout = timeout;
 	}
-	
-	
 
 }
