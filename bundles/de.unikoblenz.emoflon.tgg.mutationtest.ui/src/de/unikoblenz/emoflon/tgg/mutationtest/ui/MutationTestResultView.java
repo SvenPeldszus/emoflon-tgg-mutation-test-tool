157fb57a02a63b29f805a260002d4d52db44f4ad
Marcel Michels <m.michels@freenet.de>
2020-05-03 17:08:45 +0200

    Added base resultview

---
+++ bundles/de.unikoblenz.emoflon.tgg.mutationtest.ui/src/de/unikoblenz/emoflon/tgg/mutationtest/ui/MutationTestResultView.java
@@ -0,0 +1,20 @@
+package de.unikoblenz.emoflon.tgg.mutationtest.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MutationTestResultView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
