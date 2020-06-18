dcdc57121384769f786c2bd1a7ea5aefe2214ebb
Marcel Michels <m.michels@freenet.de>
2020-06-18 10:34:09 +0200

    Added storing for erroneous rules

--- bundles/de.unikoblenz.emoflon.tgg.mutationtest/src/de/unikoblenz/emoflon/tgg/mutationtest/util/FileHandler.java
+++ bundles/de.unikoblenz.emoflon.tgg.mutationtest/src/de/unikoblenz/emoflon/tgg/mutationtest/util/FileHandler.java
@@ -1,5 +1,6 @@
 package de.unikoblenz.emoflon.tgg.mutationtest.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,13 +23,18 @@ public class FileHandler {
 
 	private static final Path UNDETECTED_DIR_NAME = Paths.get("undetected");
 	private static final Path DETECTED_DIR_NAME = Paths.get("detected");
+	
+	private static final Path DEBUG_HOME_DIR = Paths.get(System.getProperty("user.home"), "emoflon", "debug");
 
 	private Path projectPath;
 	private Path detectedDirPath;
 	private Path undetectedDirPath;
+	
+	private String startTimeString;
 
 	public void prepareForNewRun() {
-		Path testRunDirectoryName = Paths.get("mutations", DATE_FORMAT.format(new Date()));
+		startTimeString = DATE_FORMAT.format(new Date());
+		Path testRunDirectoryName = Paths.get("mutations", startTimeString);
 
 		projectPath = MutationTestExecuter.INSTANCE.getTggProject().getLocation().toFile().toPath();
 		Path testRunDirPath = projectPath.resolve(testRunDirectoryName);
@@ -45,36 +51,39 @@ public class FileHandler {
 		Path filePath = Paths.get(mutantResult.getMutantRule().eResource().getURI().toPlatformString(true));
 		Path fileName = filePath.getFileName();
 		Path mutatedFilePath = projectPath.resolve(filePath.subpath(1, filePath.getNameCount()));
-		
+
 		Path ruleAndMutationPath = Paths.get(mutantResult.getMutantRule().getName(), mutantResult.getMutationName());
-		
-		
+
 		Path destinationDirectoryPath;
-		if(mutationDetected) {
+		if (mutationDetected) {
 			destinationDirectoryPath = detectedDirPath.resolve(ruleAndMutationPath);
 		} else {
 			destinationDirectoryPath = undetectedDirPath.resolve(ruleAndMutationPath);
 		}
-		
+
 		destinationDirectoryPath.toFile().mkdirs();
 		Path destinationPath = destinationDirectoryPath.resolve(fileName);
-		
+
+		// TODO debug
+		LOGGER.info("Saving " + (mutationDetected ? "detected" : "undetected") + " rule mutation: "
+				+ mutantResult.getMutantRule());
+		// TODO debug
+		LOGGER.info("Rule file: " + fileName);
 		try {
 			Files.copy(mutatedFilePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			LOGGER.error(e.getMessage(), e);
 		}
-		
 	}
 
 	public void createRuleFileBackup() {
-		System.out.println("Creating backup");
 		Path filePath = Paths.get(MutationTestExecuter.INSTANCE.getMutantResult().getMutantRule().eResource().getURI()
 				.toPlatformString(true));
 		Path sourcePath = projectPath.resolve(filePath.subpath(1, filePath.getNameCount()));
 		Path fileName = filePath.getFileName();
 		Path targetPath = sourcePath.resolveSibling(fileName + ".backup");
-		System.out.println(sourcePath);
+		// TODO debug
+		LOGGER.info("Creating file backup: " + filePath);
 		try {
 			Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
@@ -87,12 +96,13 @@ public class FileHandler {
 		if (mutantResult == null || mutantResult.isInitialRun() || mutantResult.getMutantRule() == null) {
 			return;
 		}
-		System.out.println("--Restoring file");
 
 		Path filePath = Paths.get(mutantResult.getMutantRule().eResource().getURI().toPlatformString(true));
 		Path fileName = filePath.getFileName();
 		Path mutatedFilePath = projectPath.resolve(filePath.subpath(1, filePath.getNameCount()));
 		Path backupFilePath = mutatedFilePath.resolveSibling(fileName + ".backup");
+		// TODO debug
+		LOGGER.info("Restoring file: " + filePath);
 
 		if (backupFilePath.toFile().exists()) {
 			try {
@@ -102,6 +112,32 @@ public class FileHandler {
 			}
 		} else {
 			System.out.println("Info: Backup file does not exist.");
+		}
+	}
+
+	public void storeMutationFileForDebug() {
+		if(!DEBUG_HOME_DIR.toFile().exists()) {
+			DEBUG_HOME_DIR.toFile().mkdirs();
+		}
+		
+		MutantResult mutantResult = MutationTestExecuter.INSTANCE.getMutantResult();
+		Path filePath = Paths.get(mutantResult.getMutantRule().eResource().getURI().toPlatformString(true));
+		Path fileName = filePath.getFileName();
+		Path mutatedFilePath = projectPath.resolve(filePath.subpath(1, filePath.getNameCount()));
+		
+		Path testRunDebugDir = DEBUG_HOME_DIR.resolve(startTimeString);
+		testRunDebugDir.toFile().mkdirs();
+		
+		Path destinationPath = testRunDebugDir.resolve(fileName);
+
+		// TODO debug
+		LOGGER.info("Saving erroneous rule mutation: " + mutantResult.getMutantRule());
+		// TODO debug
+		LOGGER.info("Rule file: " + destinationPath);
+		try {
+			Files.copy(mutatedFilePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage(), e);
 		}
 	}
 
