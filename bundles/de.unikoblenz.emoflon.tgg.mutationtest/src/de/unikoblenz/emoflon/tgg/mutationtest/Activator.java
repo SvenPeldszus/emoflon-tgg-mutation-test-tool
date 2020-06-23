8b208f774e0fc0b312d9d51889ccdf418838d823
Marcel Michels <m.michels@freenet.de>
2020-06-23 15:26:21 +0200

    Fixed logger properties

--- bundles/de.unikoblenz.emoflon.tgg.mutationtest/src/de/unikoblenz/emoflon/tgg/mutationtest/Activator.java
+++ bundles/de.unikoblenz.emoflon.tgg.mutationtest/src/de/unikoblenz/emoflon/tgg/mutationtest/Activator.java
@@ -1,6 +1,11 @@
 package de.unikoblenz.emoflon.tgg.mutationtest;
 
+import java.net.URL;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -9,4 +14,11 @@ public class Activator extends Plugin {
 	
 	// The plug-in ID
 	public static final String PLUGIN_ID = "de.unikoblenz.emoflon.tgg.mutationtest";
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		URL confURL = getBundle().getEntry("log4j.properties");
+		PropertyConfigurator.configure(FileLocator.toFileURL(confURL).getFile());
+		super.start(context);
+	}
 }
